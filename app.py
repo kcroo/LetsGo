32aa5b0f86d1a4f2c5fd33015692adaf02413e43
Kirsten Corrao <corraok@oregonstate.edu>
2019-10-24 14:48:42 -0700

    trips appear in list; link to trip pages

--- app.py
+++ app.py
@@ -8,7 +8,7 @@
 #   https://flask.palletsprojects.com/en/1.0.x/
 ##############################################################################
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from forms import NewTrip
 from config import Config 
@@ -25,10 +25,17 @@ db = MySQL(app)
 def index():
     return render_template("index.html", title="", result="Amazing trip planner!!")
 
-# new trip 
+# my trip 
 @app.route('/mytrips')
 def myTrips():
-    return render_template("mytrips.html", title="- My Trips", result="Trips appear here")
+    trips = ['Cascade Lakes', 'China', 'Oregon Coast']
+    return render_template("mytrips.html", title="- My Trips", trips=trips)
+
+# my trip 
+@app.route('/trip/<tripName>')
+def showTrip(tripName):
+    #return render_template("mytrips.html", title="- My Trips")
+    return render_template("mytrips.html", title="- My Trips", tripName=tripName)
 
 # new trip 
 @app.route('/newtrip', methods=['GET', 'POST'])
