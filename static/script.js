1e6c31b8033fbda2f11fcbb2f37e84c1ad277235
Kirsten Corrao <corraok@oregonstate.edu>
2019-10-28 16:01:48 -0700

    works, except for spaces in key name

--- static/script.js
+++ static/script.js
@@ -1,1 +1,4 @@
-console.log("i'm connected!")
+// // source: https://stackoverflow.com/questions/24148631/creating-accordion-table-with-bootstrap
+// $('.collapse').on('show.bs.collapse', function () {
+//     $('.collapse.in').collapse('hide');
+// });
