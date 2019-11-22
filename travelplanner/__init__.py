b123412b664248fef65fc211cb988fb278201b35
Kirsten Corrao <corraok@oregonstate.edu>
2019-11-21 16:00:19 -0800

    added bcrypt to app for password encryption

--- travelplanner/__init__.py
+++ travelplanner/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from .config import Config 
 from .database import Database
 
@@ -8,5 +9,8 @@ app = Flask(__name__)
 # set up database info and secret key 
 app.config.from_object(Config)
 db = Database(app)
+
+# set up bcrypt to encrypt passwords
+bcrypt = Bcrypt(app)
 
 from travelplanner import routes
