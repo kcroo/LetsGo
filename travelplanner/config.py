490a917466015f62a3f8caf072a793ef3ca8c772
Kirsten <42528453+kcroo@users.noreply.github.com>
2020-01-04 11:47:28 -0800

    Update config.py
    changed to OSU personal database

--- travelplanner/config.py
+++ travelplanner/config.py
@@ -2,7 +2,7 @@ import os, secrets
 
 class Config(object):
     SECRET_KEY = secrets.token_hex(16)
-    MYSQL_HOST = "classmysql.engr.oregonstate.edu"
-    MYSQL_USER = "cs340_corraok"
-    MYSQL_PASSWORD = "1298"
-    MYSQL_DB = "cs340_corraok"
+    MYSQL_HOST = "oniddb.cws.oregonstate.edu"
+    MYSQL_USER = "corraok-db"
+    MYSQL_PASSWORD = "oY1sLTVf7C3Z26iE"
+    MYSQL_DB = "corraok-db"
